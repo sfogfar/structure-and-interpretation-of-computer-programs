3deb142e9f29582fb2a4e9dd6e48d4523c52fdcc
samcooper720x <sam.cooper@kaluza.com>
2021-08-16 07:54:08 +0100

    ex1-22

--- exercises/chapter01/ex1-22.rkt
+++ exercises/chapter01/ex1-22.rkt
@@ -1,4 +1,6 @@
 #lang racket
+;; Racket used over SICP Scheme here for
+;; access to current-inexact-time function.
 
 (define (square x) (* x x))
 
@@ -12,7 +14,6 @@
 (define (divides? a b) (= (remainder b a) 0))
 
 (define (runtime) (current-process-milliseconds))
-;; (define (newline) (display "\n"))
 
 (define (prime? n) (= n (smallest-divisor n)))
 
@@ -23,8 +24,17 @@
 
 (define (start-prime-test n start-time)
   (if (prime? n)
-      (report-prime (- (runtime) start-time))))
+      (report-prime (- (runtime) start-time))
+      "nothing"))
 
 (define (report-prime elapsed-time)
   (display " *** ")
   (display elapsed-time))
+
+(define (search-for-primes n) (find-next-prime (+ n 1) 3 0))
+
+(define (find-next-prime n to-find found)
+  (cond ((= found to-find) "nothing")
+        ((prime? n) ((timed-prime-test n)
+                     (find-next-prime (+ n 1) to-find (+ found 1))))
+        (else (find-next-prime (+ n 1) to-find found))))
--- exercises/chapter01/todo.md
+++ exercises/chapter01/todo.md
@@ -2,4 +2,5 @@
 + [ ] Finish 1.10
 + [ ] Attempt 1.13
 + [ ] Draw out 1.14 tree
-+ [ ] Get to grips with p' q' in 1.22
++ [ ] Get to grips with p' q' in 1.19
++ [ ] Clean up 1.22, compare result timings
